38ffd9a2861cd98b9bedb74a452a2be42a99e7c5
Sion Arrowsmith <sion.arrowsmith@gmail.com>
2021-03-08 17:45:59 +0000

    UI fiddling.

---
+++ LabelledValue.gd
@@ -0,0 +1,14 @@
+extends HBoxContainer
+
+
+export(String) var label = ""
+export(String) var default = ""
+
+
+func _ready():
+	$Label.text = label
+	set_value(default)
+
+
+func set_value(value):
+	$Value.text = value
